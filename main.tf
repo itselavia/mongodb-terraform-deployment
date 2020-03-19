e3847494092a2fccb55e2e9cb7ac1d899651b3bd
Akshay Elavia <akshay.elavia@sjsu.edu>
2020-03-18 20:13:26 -0700

    Final commit

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ module "key_pair" {
   source     = "./key_pair"
   key_name   = "mongo-key-pair"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
-  }
+}
 
 module "jumpbox" {
   source    = "./jumpbox"
@@ -23,17 +23,17 @@ module "jumpbox" {
 }
 
 module "mongodb_cluster" {
-  source             = "./mongodb_cluster"
-  key_name           = "${module.key_pair.key_name}"
-  vpc_id             = "${module.vpc.vpc_id}"
-  vpc_cidr_block = "${module.vpc.vpc_cidr_block}"
-  primary_node_type = "${var.primary_node_type}"
+  source              = "./mongodb_cluster"
+  key_name            = "${module.key_pair.key_name}"
+  vpc_id              = "${module.vpc.vpc_id}"
+  vpc_cidr_block      = "${module.vpc.vpc_cidr_block}"
+  primary_node_type   = "${var.primary_node_type}"
   secondary_node_type = "${var.secondary_node_type}"
-  private_subnet_ids = "${module.vpc.private_subnet_ids}"
-  jumpbox_public_ip  = "${module.jumpbox.jumpbox_public_ip}"
-  replica_set_name = "${var.replica_set_name}"
+  private_subnet_ids  = "${module.vpc.private_subnet_ids}"
+  jumpbox_public_ip   = "${module.jumpbox.jumpbox_public_ip}"
+  replica_set_name    = "${var.replica_set_name}"
   num_secondary_nodes = "${var.num_secondary_nodes}"
-  mongo_username = "${var.mongo_username}"
-  mongo_database = "${var.mongo_database}"
-  mongo_password = "${var.mongo_password}"
+  mongo_username      = "${var.mongo_username}"
+  mongo_database      = "${var.mongo_database}"
+  mongo_password      = "${var.mongo_password}"
 }
--- vars.tf
+++ vars.tf
@@ -1,31 +1,31 @@
 variable "vpc_name" {
-  type    = "string"
+  type = "string"
 }
 
 variable "replica_set_name" {
-  
+
 }
 
 variable "num_secondary_nodes" {
-  
+
 }
 
 variable "mongo_password" {
-  
+
 }
 
 variable "mongo_username" {
-  
+
 }
 
 variable "mongo_database" {
-  
+
 }
 
 variable "primary_node_type" {
-  
+
 }
 
 variable "secondary_node_type" {
-  
+
 }
